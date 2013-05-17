54fc8c7a79e65a2789486c3cb5f8195b254cafde
machunyu <machunyu@coocox.com>
2013-05-17 16:46:22 +0800

    uln2003 cap touch

---
+++ CoX/Driver/Sensor_Touch/Cap_Touch/CapTouch_Driver/example/Touch_example/src/Example.c
@@ -0,0 +1,83 @@
+//*****************************************************************************
+//
+//! \file Example.c
+//! \brief the cap touch Example.
+//! \version 1.0
+//! \date 7/13/2012
+//! \author CooCox
+//! \copy
+//!
+//! Copyright (c)  2011, CooCox 
+//! All rights reserved.
+//! 
+//! Redistribution and use in source and binary forms, with or without 
+//! modification, are permitted provided that the following conditions 
+//! are met: 
+//! 
+//!     * Redistributions of source code must retain the above copyright 
+//! notice, this list of conditions and the following disclaimer. 
+//!     * Redistributions in binary form must reproduce the above copyright
+//! notice, this list of conditions and the following disclaimer in the
+//! documentation and/or other materials provided with the distribution. 
+//!     * Neither the name of the <ORGANIZATION> nor the names of its 
+//! contributors may be used to endorse or promote products derived 
+//! from this software without specific prior written permission. 
+//! 
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+//! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//! ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+//! LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+//! SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//! INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+//! CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+//! THE POSSIBILITY OF SUCH DAMAGE.
+//*****************************************************************************
+#include "xhw_ints.h"
+#include "xhw_nvic.h"
+#include "xhw_memmap.h"
+#include "xhw_types.h"
+#include "xdebug.h"
+#include "xcore.h"
+#include "xsysctl.h"
+#include "xhw_sysctl.h"
+#include "xhw_gpio.h"
+#include "xgpio.h"
+#include "xhw_uart.h"
+#include "xuart.h"
+#include "touch.h"
+
+#define LED  PC3 
+
+//*****************************************************************************
+//
+//! \brief The example of STMPE811.
+//!
+//! \param None
+//!
+//! \details The example of STMPE811.
+//! \return None.
+//
+//*****************************************************************************
+void TouchExample(void)
+{  
+    xSysCtlClockSet(72000000, xSYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ);
+    
+    CapTouchInit();
+  
+    xSysCtlPeripheralEnable(xGPIOSPinToPeripheralId(LED));
+    xGPIOSPinTypeGPIOOutput(LED);
+    
+    xGPIOSPinWrite(LED, 0);
+    xSysCtlDelay(xSysCtlClockGet()/4);
+    while(1)
+    {
+        if(CapTouchRead())
+            xGPIOSPinWrite(LED, 0);
+        else
+            xGPIOSPinWrite(LED, 1);
+    }
+   
+}
